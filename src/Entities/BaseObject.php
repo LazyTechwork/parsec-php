6754daf7ec8fd4da4d6cd2bdfa395727dae5213c
Ivan Petrov <lazytechwork@gmail.com>
2024-07-15 19:54:26 +0300

    Убрал рефлексию, делаем через атрибуты

--- src/Entities/BaseObject.php
+++ src/Entities/BaseObject.php
@@ -2,65 +2,117 @@
 
 namespace LazyTechwork\Parsec\Entities;
 
-use LazyTechwork\Parsec\ReflectionHelper;
-use LazyTechwork\Parsec\XmlSerializable;
 use Ramsey\Uuid\Guid\Guid;
 use Ramsey\Uuid\UuidInterface;
 
-abstract class BaseObject implements XmlSerializable
+abstract class BaseObject
 {
-    public static function fromStdClass(\stdClass $obj): static
+    /**
+     * @var array<string, mixed>
+     */
+    protected array $attributes = [];
+    /**
+     * @var array<string, string> stdClass -> this
+     */
+    protected array $attributeMapping = [];
+
+    /**
+     * @return array<string, string|class-string|callable>
+     */
+    protected function casts(): array
     {
-        $constructor = (new \ReflectionClass(static::class))->getConstructor();
-        if ($constructor === null) {
-            throw new \RuntimeException('Cannot instantiate class '.static::class);
-        }
-        $params = $constructor->getParameters();
-        $resultParams = [];
-        foreach ($params as $param) {
-            if (isset($obj->{$param->getName()})) {
-                $types = ReflectionHelper::getAllTypes($param);
-                if (($parsedParam = static::parseParam($param->getName(), $obj->{$param->getName()})) !== false) {
-                    $resultParams[$param->getName()] = $parsedParam;
-                } elseif (ReflectionHelper::hasType($types, UuidInterface::class)) {
-                    $resultParams[$param->getName()] = static::createFromGuidString($obj->{$param->getName()});
-                } elseif (ReflectionHelper::hasType($types, \DateTimeInterface::class)) {
-                    $resultParams[$param->getName()] = \DateTimeImmutable::createFromFormat(\DateTimeInterface::ATOM, $obj->{$param->getName()});
-                } else {
-                    $resultParams[$param->getName()] = $obj->{$param->getName()};
-                }
-                continue;
-            }
-
-            if ($param->isOptional()) {
-                continue;
-            }
-
-            if ($param->isDefaultValueAvailable()) {
-                $resultParams[$param->getName()] = $param->getDefaultValue();
-                continue;
-            }
-
-            if ($param->allowsNull()) {
-                $resultParams[$param->getName()] = null;
-            }
-        }
-
-        return new static(...$resultParams);
+        return [];
     }
 
-    protected static function parseParam(string $paramName, mixed $value): mixed
+    protected function castAttribute(string $key, mixed $value): mixed
     {
-        return false;
+        $casts = $this->casts();
+
+        if (array_key_exists($key, $casts)) {
+            if ($casts['key'] instanceof \Closure) {
+                return $casts['key']($value);
+            }
+
+            $castType = $casts[$key];
+        } else {
+            $castType = 'string';
+        }
+
+        switch ($castType) {
+            case 'int':
+            case 'integer':
+                return (int) $value;
+            case 'real':
+            case 'float':
+            case 'double':
+                return $this->fromFloat($value);
+            case 'string':
+                return (string) $value;
+            case 'bool':
+            case 'boolean':
+                return (bool) $value;
+            case 'datetime':
+                return $this->asDateTime($value);
+            case 'guid':
+                return $this->fromGuidString((string) $value);
+        }
+
+        try {
+            $reflected = new \ReflectionClass($castType);
+            if ($reflected->isSubclassOf(self::class)) {
+                /** @var BaseObject $class */
+                $class = new ($reflected->getName())();
+
+                return $class->fill($value);
+            }
+        } catch (\ReflectionException) {
+        }
+
+        return $value;
     }
 
-    protected static function makeStringSoapVar(string $value): \SoapVar
+    /**
+     * @param \stdClass|array<string, mixed> $values
+     *
+     * @return $this
+     *
+     * @throws \Exception
+     */
+    public function fill(\stdClass|array $values): static
+    {
+        if ($values instanceof \stdClass) {
+            $values = get_object_vars($values);
+        }
+
+        foreach ($values as $key => $value) {
+            $this->attributes[$this->attributeMapping[$key] ?? $key] = $this->castAttribute($this->attributeMapping[$key] ?? $key, $value);
+        }
+
+        return $this;
+    }
+
+    public function makeStringSoapVar(string $value): \SoapVar
     {
         return new \SoapVar($value, XSD_STRING, 'string', XSD_NAMESPACE);
     }
 
-    protected static function createFromGuidString(string $guid): UuidInterface
+    public function fromGuidString(string $guid): UuidInterface
     {
         return Guid::fromString($guid);
+    }
+
+    public function fromFloat(mixed $value): float
+    {
+        return match ((string) $value) {
+            'Infinity' => INF,
+            '-Infinity' => -INF,
+            'NaN' => NAN,
+            default => (float) $value,
+        };
+    }
+
+    public function asDateTime(mixed $value): \DateTimeInterface
+    {
+        return \DateTime::createFromFormat(\DateTimeInterface::ATOM, (string) $value, new \DateTimeZone('UTC'));
     }
 }
